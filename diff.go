39f7208e38d93aa6bcd77591c6c5cde5890e4a7d
Ben Muthalaly <benjamin.muthalaly@gmail.com>
2020-07-17 11:33:49 -0400

    add shortestEdit function

--- diff.go
+++ diff.go
@@ -27,6 +27,40 @@ func breakup(s string) []line {
 	return lines
 }
 
+func shortestEdit(a string, b string) int {
+	aLines := breakup(a)
+	bLines := breakup(b)
+
+	n, m := len(a), len(b)
+	max := n + m
+
+	v := make([]int, 2*max+1)
+	v[0] = 0
+
+	var fewestEdits int
+	for d := 0; d < max; d++ {
+		for k := -d; k < d; k += 2 {
+			var x int
+			if k == -d || (k != d && v[k-1] < v[k+1]) {
+				x = v[k+1]
+			} else {
+				x = v[k-1] + 1
+			}
+			y := x - k
+
+			for x < n && y < m && aLines[x].text == bLines[y].text {
+				x, y = x+1, y+1
+			}
+			v[k] = x
+
+			if x >= n && y >= m {
+				fewestEdits = d
+			}
+		}
+	}
+	return fewestEdits
+}
+
 func main() {
 	a := "ABCABBA"
 	b := "CBABAC"
